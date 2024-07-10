287ef75551dff9e493a5f91961718b023c7295c1
shumaila05 <shahinaaqibkhan@gmail.com>
2024-07-10 15:29:15 +0500

    while loops
    while loops & factorial

---
+++ javascript/script.js
@@ -0,0 +1,66 @@
+// let n = +prompt("Enter the value of n");
+// let i = 0;
+// while(i<=n){
+//     document.write("<h2>" + i + "<br> </h2>");
+//     i++;
+// }
+// sum of numbers
+let sum = 0;
+let num = 1;
+while(num<=100){
+sum += num; // sum = sum + num
+num++;
+}
+document.write("Sum of 1 to 100 is " + "<strong>" + sum + "</strong>");
+//factorial of number
+let number = 4;
+let factorial = 1;
+while(number>0){
+    factorial = factorial * number; // 4,12,24
+    number --;(1)
+}
+document.write("factorial of number is:" + "<strong>"+factorial + "</strong>");
+// even Number //
+
+let = evenNumber =[]
+let i = 1 ;
+while(evenNumber.length<10){
+    if(i%2===0){
+        evenNumber.push(i)
+    }
+    i++;
+}
+document.write("<br> first 10 even number is :"+"<strong>" + evenNumber+"</strong>");
+
+// odd Number //
+// let = oddNumber =[]
+// let i = 0;
+// while(oddNumber.length<10){
+//     if(i%2===0){
+//         onloadeddataNumber.push(i)
+//     }
+//     i++;
+// }
+// document.write("<br> first 10 odd number is :"+"<strong>" + evenNumber+"</strong>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
